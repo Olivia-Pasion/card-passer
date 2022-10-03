4071fbe8e30b7b4f7b6fe39fcbdfa809ab9ac77a
olivia pasion <oap9820@gmail.com>
2022-10-03 11:43:31 -0700

    added Miro Board to README.md

---
+++ src/context/GameContext.js
@@ -0,0 +1,29 @@
+import { createContext, useContext, useState } from 'react';
+
+const GameContext = createContext();
+
+const GameProvider = ({ children }) => {
+  const [deck, setDeck] = useState([]);
+  const [playerOne, setPlayerOne] = useState([]);
+  const [playerTwo, setPlayerTwo] = useState([]);
+  const [playerThree, setPlayerThree] = useState([]);
+  
+
+  return (
+    <GameContext.Provider value={{ deck, setDeck }}>
+      {children}
+    </GameContext.Provider>
+  );
+};
+
+const useGameContext = () => {
+  const context = useContext(GameContext);
+
+  if (context === undefined) {
+    throw new Error("useGameContext must be used within GameProvider");
+  }
+
+  return context;
+};
+
+export { GameProvider, useGameContext };
