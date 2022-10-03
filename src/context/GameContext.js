47ada9d207cc203226778828b00942efdbd0438b
olivia pasion <oap9820@gmail.com>
2022-10-03 12:57:32 -0700

    added more state to GameContext.js

--- src/context/GameContext.js
+++ src/context/GameContext.js
@@ -1,16 +1,28 @@
 import { createContext, useContext, useState } from 'react';
+import initialCards from '../cards-data';
 
 const GameContext = createContext();
 
 const GameProvider = ({ children }) => {
-  const [deck, setDeck] = useState([]);
-  const [playerOne, setPlayerOne] = useState([]);
-  const [playerTwo, setPlayerTwo] = useState([]);
-  const [playerThree, setPlayerThree] = useState([]);
+  const [deck, setDeck] = useState(initialCards);
+  const [playerOneHand, setPlayerOneHand] = useState([]);
+  const [playerTwoHand, setPlayerTwoHand] = useState([]);
+  const [playerThreeHand, setPlayerThreeHand] = useState([]);
+  const [from, setFrom] = useState('deck');
+  const [to, setTo] = useState(1);
+  const [selectedCard, setSelectedCard] = useState();
   
 
   return (
-    <GameContext.Provider value={{ deck, setDeck }}>
+    <GameContext.Provider value={{ 
+      deck, setDeck, 
+      playerOneHand, setPlayerOneHand,
+      playerTwoHand, setPlayerTwoHand,
+      playerThreeHand, setPlayerThreeHand,
+      from, setFrom,
+      to, setTo,
+      selectedCard, setSelectedCard
+    }}>
       {children}
     </GameContext.Provider>
   );
